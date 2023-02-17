0637c626a31e24a061758ad1cdd80b2782dbd45a
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-02-17 17:22:27 +0100

    add bootstrap and scss + Form

--- laravel-api/vue-laravel-microservice/src/App.vue
+++ laravel-api/vue-laravel-microservice/src/App.vue
@@ -58,6 +58,40 @@ export default{
 
 <template>
     <h1>Movies</h1>
+
+    <form>
+
+      <div class="card p-3">
+        <h6 class="text-dark">Form</h6>
+        <div class="d-flex flex-column gap-2">
+            <input type="text" name="name" id="name" placeholder="name">
+            <input type="number" name="year" id="year" placeholder="year">
+            <input type="number" name="cashOut" id="cashOut" placeholder="cashOut">
+            
+            <!-- Genre -->
+            <!-- <select name="genre_id" id="genre_id">
+
+              <option value="{{$genre -> id}}">{{$genre -> name}}</option>
+
+            </select> -->
+
+            <div>
+
+              <!-- Tags -->
+              <!-- <input type="checkbox" name="tags_id[]" id="{{$tag -> id}}" value="{{$tag -> id}}">
+              <label for="{{$tag -> id}}">{{$tag -> name}}</label>
+              <br> -->
+
+            </div>
+            <div class="d-flex gap-3">
+              <input type="submit" value="Cancel">
+              <input class="flex-grow-1" type="submit" value="Create">
+            </div>
+        </div>
+      </div>    
+
+    </form>
+
     <ul>
       <li v-for="movie in movies" :key="movie.id">
         {{ movie.name }}
@@ -67,31 +101,7 @@ export default{
     </ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style lang="scss">
+@use './styles/general.scss' as *;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
